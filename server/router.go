7fc17557e1fc4fa91e92f888d12fa301b8fd94a5
Emircan Tepe <emircan.tepe@ogr.deu.edu.tr>
2022-11-30 08:46:42 +0300

    adapt new DB design to go

--- server/router.go
+++ server/router.go
@@ -6,10 +6,7 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/log_student/{username}/{password}", student_log_in)
-	//router.HandleFunc("/log_lecturer/{username}/{password}", lecturer_log_in)
-	router.HandleFunc("/student_forgot/{username}", student_forgot)
-	router.HandleFunc("/get_gen_announcements", get_general_announcements)
-	router.HandleFunc("/get_course_announcements", handler_get_course_announcements)
-	router.HandleFunc("/get_courses", handler_get_courses)
+	router.HandleFunc("", get_general_announcements)
+
 	return router
 }

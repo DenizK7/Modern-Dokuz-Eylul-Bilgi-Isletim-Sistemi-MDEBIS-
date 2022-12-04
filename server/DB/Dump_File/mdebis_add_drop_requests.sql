e2c0197d46625ddea37df849cadac3c4050002f8
Emircan Tepe <emircan.tepe@ogr.deu.edu.tr>
2022-12-04 20:45:55 +0300

    add updated DB dump

---
+++ server/DB/Dump_File/mdebis_add_drop_requests.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mdebis
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `add_drop_requests`
+--
+
+DROP TABLE IF EXISTS `add_drop_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `add_drop_requests` (
+  `Request_Id` int NOT NULL,
+  `Student_Id` int NOT NULL,
+  `Responsible_Lecturer_Id` int NOT NULL,
+  PRIMARY KEY (`Request_Id`,`Student_Id`,`Responsible_Lecturer_Id`),
+  KEY `fk_Add_Drop_Requests_Student1_idx` (`Student_Id`),
+  KEY `fk_Add_Drop_Requests_Lecturer1_idx` (`Responsible_Lecturer_Id`),
+  CONSTRAINT `fk_Add_Drop_Requests_Lecturer1` FOREIGN KEY (`Responsible_Lecturer_Id`) REFERENCES `lecturer` (`Lecturer_Id`),
+  CONSTRAINT `fk_Add_Drop_Requests_Student1` FOREIGN KEY (`Student_Id`) REFERENCES `student` (`Student_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `add_drop_requests`
+--
+
+LOCK TABLES `add_drop_requests` WRITE;
+/*!40000 ALTER TABLE `add_drop_requests` DISABLE KEYS */;
+INSERT INTO `add_drop_requests` VALUES (1,1,1);
+/*!40000 ALTER TABLE `add_drop_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-04 20:43:36

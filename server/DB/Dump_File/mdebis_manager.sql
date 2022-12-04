9e52bf854d6ce00ff4c69cc8807aa0d132294b60
Emircan Tepe <emircan.tepe@ogr.deu.edu.tr>
2022-12-04 21:04:38 +0300

    update DB dump

--- server/DB/Dump_File/mdebis_manager.sql
+++ server/DB/Dump_File/mdebis_manager.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `mdebis` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mdebis`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: mdebis
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-04 20:43:34
+-- Dump completed on 2022-12-04 21:04:11
